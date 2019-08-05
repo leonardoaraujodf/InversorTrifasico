d75146a59cb59e213ec2e3afeb562ddb04dd8915
Leonardo Amorim <leonardoaraujodf@gmail.com>
2019-08-05 12:50:29 -0300

    code for PWM updated

---
+++ Inversor_Trifasico/Debug/main2.d
@@ -0,0 +1,65 @@
+# FIXED
+
+main2.obj: ../main2.c
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_Device.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_Adc.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_DevEmu.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_CpuTimers.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_ECan.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_ECap.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_DMA.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_EPwm.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_EQep.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_Gpio.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_I2c.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_McBSP.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_PieCtrl.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_PieVect.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_Spi.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_Sci.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_SysCtrl.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_XIntrupt.h
+main2.obj: C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_Xintf.h
+main2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP28x_Project.h
+main2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_Examples.h
+main2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_GlobalPrototypes.h
+main2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_ePwm_defines.h
+main2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_Dma_defines.h
+main2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_I2C_defines.h
+main2.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_DefaultISR.h
+main2.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include/Math.h
+main2.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include/_ti_config.h
+main2.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include/linkage.h
+main2.obj: C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include/_defs.h
+
+../main2.c: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_Device.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_Adc.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_DevEmu.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_CpuTimers.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_ECan.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_ECap.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_DMA.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_EPwm.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_EQep.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_Gpio.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_I2c.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_McBSP.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_PieCtrl.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_PieVect.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_Spi.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_Sci.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_SysCtrl.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_XIntrupt.h: 
+C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include/DSP2833x_Xintf.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP28x_Project.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_Examples.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_GlobalPrototypes.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_ePwm_defines.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_Dma_defines.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_I2C_defines.h: 
+C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include/DSP2833x_DefaultISR.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include/Math.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include/_ti_config.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include/linkage.h: 
+C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include/_defs.h: 
---
+++ Inversor_Trifasico/Debug/subdir_rules.mk
@@ -0,0 +1,85 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Each subdirectory must supply rules for building sources it contributes
+DSP2833x_ADC_cal.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/source/DSP2833x_ADC_cal.asm $(GEN_OPTS) | $(GEN_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="C:/Users/Leonardo/DSP2833x/labs/Inversor_Trifasico" --include_path="C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include" --include_path="C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include" --include_path="C:/ti/controlSUITE/device_support/f2833x/v142/DSP2833x_common/include" --include_path="C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+DSP2833x_Adc.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/source/DSP2833x_Adc.c $(GEN_OPTS) | $(GEN_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="C:/Users/Leonardo/DSP2833x/labs/Inversor_Trifasico" --include_path="C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include" --include_path="C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include" --include_path="C:/ti/controlSUITE/device_support/f2833x/v142/DSP2833x_common/include" --include_path="C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+DSP2833x_CodeStartBranch.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/source/DSP2833x_CodeStartBranch.asm $(GEN_OPTS) | $(GEN_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="C:/Users/Leonardo/DSP2833x/labs/Inversor_Trifasico" --include_path="C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include" --include_path="C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include" --include_path="C:/ti/controlSUITE/device_support/f2833x/v142/DSP2833x_common/include" --include_path="C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+DSP2833x_CpuTimers.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/source/DSP2833x_CpuTimers.c $(GEN_OPTS) | $(GEN_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="C:/Users/Leonardo/DSP2833x/labs/Inversor_Trifasico" --include_path="C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include" --include_path="C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include" --include_path="C:/ti/controlSUITE/device_support/f2833x/v142/DSP2833x_common/include" --include_path="C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+DSP2833x_DefaultIsr.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/source/DSP2833x_DefaultIsr.c $(GEN_OPTS) | $(GEN_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="C:/Users/Leonardo/DSP2833x/labs/Inversor_Trifasico" --include_path="C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include" --include_path="C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include" --include_path="C:/ti/controlSUITE/device_support/f2833x/v142/DSP2833x_common/include" --include_path="C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+DSP2833x_GlobalVariableDefs.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_headers/source/DSP2833x_GlobalVariableDefs.c $(GEN_OPTS) | $(GEN_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="C:/Users/Leonardo/DSP2833x/labs/Inversor_Trifasico" --include_path="C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include" --include_path="C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include" --include_path="C:/ti/controlSUITE/device_support/f2833x/v142/DSP2833x_common/include" --include_path="C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+DSP2833x_PieCtrl.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/source/DSP2833x_PieCtrl.c $(GEN_OPTS) | $(GEN_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="C:/Users/Leonardo/DSP2833x/labs/Inversor_Trifasico" --include_path="C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include" --include_path="C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include" --include_path="C:/ti/controlSUITE/device_support/f2833x/v142/DSP2833x_common/include" --include_path="C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+DSP2833x_PieVect.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/source/DSP2833x_PieVect.c $(GEN_OPTS) | $(GEN_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="C:/Users/Leonardo/DSP2833x/labs/Inversor_Trifasico" --include_path="C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include" --include_path="C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include" --include_path="C:/ti/controlSUITE/device_support/f2833x/v142/DSP2833x_common/include" --include_path="C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+DSP2833x_SysCtrl.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/source/DSP2833x_SysCtrl.c $(GEN_OPTS) | $(GEN_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="C:/Users/Leonardo/DSP2833x/labs/Inversor_Trifasico" --include_path="C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include" --include_path="C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include" --include_path="C:/ti/controlSUITE/device_support/f2833x/v142/DSP2833x_common/include" --include_path="C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+DSP2833x_usDelay.obj: C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/source/DSP2833x_usDelay.asm $(GEN_OPTS) | $(GEN_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="C:/Users/Leonardo/DSP2833x/labs/Inversor_Trifasico" --include_path="C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include" --include_path="C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include" --include_path="C:/ti/controlSUITE/device_support/f2833x/v142/DSP2833x_common/include" --include_path="C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+%.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES)
+	@echo 'Building file: "$<"'
+	@echo 'Invoking: C2000 Compiler'
+	"C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/bin/cl2000" -v28 -ml -mt --float_support=fpu32 --include_path="C:/Users/Leonardo/DSP2833x/labs/Inversor_Trifasico" --include_path="C:/tidcs/C28/dsp2833x/v131/DSP2833x_headers/include" --include_path="C:/tidcs/c28/DSP2833x/v131/DSP2833x_common/include" --include_path="C:/ti/controlSUITE/device_support/f2833x/v142/DSP2833x_common/include" --include_path="C:/ti/ccs901/ccs/tools/compiler/ti-cgt-c2000_18.12.1.LTS/include" -g --diag_warning=225 --diag_wrap=off --display_error_number --abi=coffabi --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	@echo 'Finished building: "$<"'
+	@echo ' '
+
+
